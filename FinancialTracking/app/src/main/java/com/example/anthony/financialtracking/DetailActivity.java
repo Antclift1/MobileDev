8dbee67f268be0b18536bb73483edbd15a9ee253
MattAMarquez <33560698+MattAMarquez@users.noreply.github.com>
2018-04-28 12:33:41 -0400

    Fixing Detail screen

--- FinancialTracking/app/src/main/java/com/example/anthony/financialtracking/DetailActivity.java
+++ FinancialTracking/app/src/main/java/com/example/anthony/financialtracking/DetailActivity.java
@@ -2,12 +2,27 @@ package com.example.anthony.financialtracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DetailActivity extends AppCompatActivity {
-
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener{
+    Button editExpense;
+    Button deleteExpense;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        editExpense = (Button)findViewById(R.id.editExpense);
+        deleteExpense = (Button)findViewById(R.id.deleteExpense);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == editExpense.getId()){
+
+        }
+        else if(view.getId() == deleteExpense.getId()){
+
+        }
     }
 }
