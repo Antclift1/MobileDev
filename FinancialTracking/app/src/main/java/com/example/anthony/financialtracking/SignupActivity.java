5eefd81eb2e0b832b0b40b65205b8eb7cc0c8e3e
antclift1 <antclift@vt.edu>
2018-05-01 17:25:38 -0400

    Saves login info

--- FinancialTracking/app/src/main/java/com/example/anthony/financialtracking/SignupActivity.java
+++ FinancialTracking/app/src/main/java/com/example/anthony/financialtracking/SignupActivity.java
@@ -3,8 +3,11 @@ package com.example.anthony.financialtracking;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.regex.Pattern;
@@ -13,17 +16,31 @@ public class SignupActivity extends Activity implements LoginResponse{
 
 
     static final int REGISTER_REQUEST_CODE = 1;
-    private EditText usernameField,passwordField,passwordField2;
+    private EditText emailField,usernameField,passwordField,passwordField2;
+    private EditText firstField, secondField, dobField;
+    private RadioGroup genderGroup;
     private String user;
+
+
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        emailField = (EditText)findViewById(R.id.SignupEmail);
         usernameField = (EditText)findViewById(R.id.SignupUser);
         passwordField = (EditText)findViewById(R.id.SignupPass);
         passwordField2 = (EditText)findViewById(R.id.SignupConfirm);
+
+        firstField = (EditText)findViewById(R.id.SignupFirst);
+        secondField = (EditText)findViewById(R.id.SignupLast);
+        dobField = (EditText)findViewById(R.id.SignupDOB);
+
+        genderGroup = (RadioGroup)findViewById(R.id.GenderGroup);
+
+        new DateInputMask(dobField);
 
 
     }
@@ -40,19 +57,35 @@ public class SignupActivity extends Activity implements LoginResponse{
     //submit the signup information
     public void submit(View view){
         String username = usernameField.getText().toString();
+        String email = emailField.getText().toString();
+        int selectedGender = genderGroup.getCheckedRadioButtonId();
+        radioButton = (RadioButton) findViewById(selectedGender);
+
+
         Pattern p = Pattern.compile("[^a-zA-Z0-9]");
         boolean usernameSpecialChar = p.matcher(username).find();
-        if(usernameSpecialChar){
-            Toast.makeText(this, "Username must be alphanumeric.", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            if (passwordField.getText().toString().equals(passwordField2.getText().toString()) ){
-                String password = passwordField.getText().toString();
 
-                new Signup(this).execute(username, password);
-            } else {
+        boolean validEmail = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
+        if(!validEmail){
+            Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+        }
+        else if(usernameSpecialChar){
+            Toast.makeText(this, "Username must be alphanumeric.", Toast.LENGTH_SHORT).show();
+
+
+            String dob = dobField.getText().toString();
+
+            Toast.makeText(this, dob, Toast.LENGTH_SHORT).show();
+        }
+        else if (!passwordField.getText().toString().equals(passwordField2.getText().toString())){
                 Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
-            }
+        }
+        else{
+            String gender = radioButton.getText().toString();
+            String password = passwordField.getText().toString();
+            new Signup(this).execute(username, password);
+
         }
 
     }
